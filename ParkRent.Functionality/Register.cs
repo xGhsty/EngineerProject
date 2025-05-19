3a3fcd125d58002918c993f22160ee1e42fc480f
KrystianLabak <krystian.labak2002@gmail.com>
2025-05-19 18:35:44 +0200

    Preparing Login and Register functionality (for now, just added Login and Register entities

---
+++ ParkRent.Functionality/Register.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkRent.Functionality
+{
+    public class Register
+    {   
+        public required string? FirstName { get; set; }
+        public required string? LastName { get; set; }
+        public required string? Email { get; set; }
+        public required string? Password { get; set; }
+        public int PhoneNumber { get; set; }
+
+    }
+}
